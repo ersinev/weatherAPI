c9dd9b950f669149ca737212a8d6e3692cc7fb70
ersinev <ersinevci@hotmail.com>
2023-03-23 17:24:18 +0100

    news.tsx updated

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,33 +1,86 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import Button from '@mui/material/Button';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Button from "@mui/material/Button";
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 function News() {
-  const [news, setNews] = useState([]);
- 
-  const getdata = ()=>{
-    var url = 'https://api.currentsapi.services/v1/latest-news?' +
-    'language=us&' +
-    'apiKey=D7Wi4B_TOhWbjcmznf_LJVTt3f7OQ2_rmw7pEtDkKFma7czz';
-    var req = new Request(url);
-    fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
-  }
-  
+  const [singleTopic, setsingleTopic] = useState("");
+  const [news, setnews] = useState([] as any);
+  const getdata = async (topic: any) => {
+    let res = await fetch(
+      `https://newsapi.org/v2/everything?q=${topic}&from=2023-02-23&sortBy=publishedAt&apiKey=c0936d28ce4548bd94f3b38fc87554d1`
+    );
+    let data = await res.json();
+    setnews(data.articles);
+    console.log(data.articles);
+    console.log(news);
+  };
 
   return (
     <>
-     
-      <Button onClick={()=>{getdata()}}>Get data</Button>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="outlined-basic"
+          label="Enter City Name"
+          variant="outlined"
+          onChange={(e: any) => setsingleTopic(e.target.value)}
+        />
+      </Box>
+
+      <Button
+        variant="contained"
+        onClick={() => {
+          getdata(singleTopic);
+        }}
+      >
+        Get data
+      </Button>
+
+      <Grid container >
+        {news &&
+          news.map((eachnew: any) => {
+            return (
+              <Card sx={{ maxWidth: 345, margin:'5px' }}>
+                <CardMedia
+                  sx={{ height: 140 }}
+                  image={eachnew.urlToImage}
+                  title="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {eachnew.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {eachnew.content}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" href={eachnew.url} target="_blank">
+                    Read more
+                  </Button>
+                </CardActions>
+              </Card>
+            );
+          })}
+      </Grid>
     </>
   );
 }
 
 export default News;
-
-
-
-
-
