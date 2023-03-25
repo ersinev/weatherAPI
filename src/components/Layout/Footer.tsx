89a65d8126de6ffebdeb99f17a60c59141f8e3ed
ersinev <ersinevci@hotmail.com>
2023-03-25 16:57:15 +0100

    footer updated

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -20,33 +20,33 @@ function Copyright() {
 
 export default function Footer() {
   return (
-    // <Box
-    //   sx={{
-    //     display: 'flex',
-    //     flexDirection: 'column',
-    //     minHeight: 'auto',
-    //   }}
-    // >
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: 'auto',
+      }}
+    >
       
-    //   <Box
-    //     component="footer"
-    //     sx={{
-    //       py: 3,
-    //       px: 2,
-    //       mt: 'auto',
-    //       backgroundColor: (theme) =>
-    //         theme.palette.mode === 'light'
-    //           ? theme.palette.grey[200]
-    //           : theme.palette.grey[800],
-    //     }}
-    //   >
-    //     <Container maxWidth="sm">
-    //       <Typography variant="body1">
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: 'auto',
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800],
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography variant="body1">
             
-    //       </Typography>
-    //       <Copyright />
-    //     </Container>
-    //   </Box>
-    // </Box>
+          </Typography>
+          <Copyright />
+        </Container>
+      </Box>
+    </Box>
   );
 }
